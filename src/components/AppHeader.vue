739ce9fab2a4d5225dcf601406ffa052a24b838d
Fede180600 <fede180600@libero.it>
2022-05-19 16:33:56 +0200

    AppHeader done

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+header {
+    height: 60px;
+    background-color: $app-light-gray;
+}
+</style>
