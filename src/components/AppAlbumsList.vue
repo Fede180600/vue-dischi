4b2dffadc6881b2e7332dd85e35b9edc92565949
Fede180600 <fede180600@libero.it>
2022-05-20 10:41:51 +0200

    add apploader

--- src/components/AppAlbumsList.vue
+++ src/components/AppAlbumsList.vue
@@ -1,8 +1,15 @@
 <template>
-    <section class="albums_list">
-        <div class="container p-5">
-            <div class="row row-cols-3 row-cols-md-6 justify-content-center">
-                <AppAlbum v-for="(card, index) in albums" :key="index" :album="card"/>
+    <section>
+        <div v-if="loading" class="container my_load">
+        <AppLoading />
+        </div>
+        <div v-else>
+            <div class="albums_list py-4">
+                <div class="container">
+                    <div class="row row-cols-3 row-cols-md-6 justify-content-center">
+                        <AppAlbum v-for="(card, index) in albums" :key="index" :album="card"/>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
@@ -10,16 +17,19 @@
 
 <script>
 import AppAlbum from "./AppAlbum.vue";
+import AppLoading from "./AppLoading.vue";
 import axios from "axios";
 
 export default {
     name: "AppAlbumsList",
     components: {
         AppAlbum,
+        AppLoading,
     },
     data: function() {
         return {
             albums: [],
+            loading: true,
         };
     },
     created() {
@@ -27,6 +37,7 @@ export default {
         .get("https://flynn.boolean.careers/exercises/api/array/music")
         .then((resp) => {
             this.albums = resp.data.response;
+            this.loading = false;
         });
     }
 
@@ -34,7 +45,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.albums_list {
-    height: 80%;
+.my_load {
+    color: white;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 }
 </style>
---
+++ src/components/AppLoading.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        LOADING
+    </div>
+</template>
+<script>
+export default {
+    NAME: "AppLoading",
+}
+</script>
+<style lang="scss" scoped>
+div {
+    text-align: center;
+}
+</style>
